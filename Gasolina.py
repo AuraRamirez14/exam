1c8e2f1a3e2d8b34cf08e0fdf146d9d5c9eddf4a
AuraRamirez14 <auramil.amrp@gmail.com>
2025-09-20 21:06:20 -0600

    Update Gasolina.py

--- Gasolina.py
+++ Gasolina.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 
 st.tittle(''' # Estimacion del precio de la gasolina en funcion de estado, mes y año ''')
-st.image("gasolina.png", caption="Gasolina.",width="100")
+st.image("gasolina.png", caption="Gasolina.")
 #st.image("tabla.png", caption="Tabla de equivalencia de estados.")
 
 st.header('Datos de referncia')
